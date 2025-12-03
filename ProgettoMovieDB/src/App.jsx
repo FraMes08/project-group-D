c0fe5682729ee576b68015704de0850ffd7a2534
Riccardo V. <ricc.vargiu@gmail.com>
2025-12-03 17:21:03 +0100

    Implement MovieCard component with styling and details toggle

--- ProgettoMovieDB/src/App.jsx
+++ ProgettoMovieDB/src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
+import "./App.css";
+import MovieCard from "./components/MovieCard/MovieCard.jsx";
 
 function App() {
-
   return (
     <>
-      
-
-      
+      <MovieCard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
