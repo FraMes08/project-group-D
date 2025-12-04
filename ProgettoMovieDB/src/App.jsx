10731658465aa620358534ca0dc83df5f55384fc
Italo Coppolella <italo7094@hotmail.it>
2025-12-04 17:39:33 +0100

    Realizzati: Logo, Footer

--- ProgettoMovieDB/src/App.jsx
+++ ProgettoMovieDB/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import TMDBFetcher from './components/TMDBFetcher/TMDBFetcher.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
       <TMDBFetcher />
+      <Footer />
     </>
   )
 }
