f51f07018db65a03006692397581b4c1b3a852b0
Riccardo V. <ricc.vargiu@gmail.com>
2025-12-04 17:41:06 +0100

    feat: Implement Header component with styling and buttons

--- ProgettoMovieDB/src/App.jsx
+++ ProgettoMovieDB/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import FetchData from "./components/TMDBFetcher/TMDBFetcher.jsx";
+import Header from "./components/Atoms/Header/Header.jsx";
 // import Fetch from "./components/FetchData/Fetch.jsx";
 
 function App() {
   return (
     <>
+      <Header />
       <FetchData />
     </>
   );
